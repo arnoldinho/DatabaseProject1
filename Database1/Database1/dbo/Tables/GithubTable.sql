b5105b8f72837cf00a9284178ab1a8e6a569eb17
a.nowak <a.nowak@PK5-06>
2019-07-29 18:48:13 +0200

    import database

---
+++ Database1/Database1/dbo/Tables/GithubTable.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[GithubTable] (
+    [id]      INT           IDENTITY (1, 1) NOT NULL,
+    [column1] VARCHAR (100) NULL,
+    [column2] VARCHAR (200) NULL,
+    PRIMARY KEY CLUSTERED ([id] ASC)
+);
+
